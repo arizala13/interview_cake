3f59e6bd52a05088f6515d9f70ee4650cb077bf4
Andres Arizala <arizala13@gmail.com>
2020-10-01 14:29:08 -0500

    "Setting up for cake_shop.py question"

---
+++ cake_shop.py
@@ -0,0 +1,19 @@
+def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
+
+    # Check if we're serving orders first-come, first-served
+    print("These are the take_out_orders", take_out_orders)
+    print("These are the dine_in_orders", dine_in_orders)
+    print("These are the served_orders", served_orders)
+
+    return False
+
+
+is_first_come_first_served([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6])
+
+
+# --- notes about the question ---
+#
+#
+#
+#
+#
