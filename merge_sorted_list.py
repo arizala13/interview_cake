4b897b1107ca0a6df62979a35e6d1f5075a0e1b7
Andres Arizala <arizala13@gmail.com>
2020-10-01 11:49:47 -0500

    "used builtin sort, but we can do better"

--- merge_sorted_list.py
+++ merge_sorted_list.py
@@ -2,9 +2,16 @@ my_list = [3, 4, 6, 10, 11, 15]
 alices_list = [1, 5, 8, 12, 14, 19]
 
 
+# this works, but is in O(n lg n) time which
+# is not very good
+def merge_lists_slow(list1, list2):
+    list1 = list1 + list2
+    print(sorted(list1))
+
+
+# An alternative is
 def merge_lists(list1, list2):
-    print(my_list)
-    print(alices_list)
+
 
 
 merge_lists(my_list, alices_list)
