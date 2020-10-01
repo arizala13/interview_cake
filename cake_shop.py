e969c5ff3bb99796c3b96130b32b27af43290439
Andres Arizala <arizala13@gmail.com>
2020-10-01 15:29:21 -0500

    "Notes for question and approach"

--- cake_shop.py
+++ cake_shop.py
@@ -1,19 +1,34 @@
 def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
+    # how do we assign values to each order?
 
-    # Check if we're serving orders first-come, first-served
-    print("These are the take_out_orders", take_out_orders)
-    print("These are the dine_in_orders", dine_in_orders)
-    print("These are the served_orders", served_orders)
+    # Assign values to each order
 
-    return False
+    # compare to see if the values are
+    # increasing order or same
+    # ex: [1, 2, 4, 4, 5, 6]
+    # we look at 1, 2, 4, etc. Once all have
+    # been seen we return True
+    # ex: [1, 2, 7, 4, 5, 6]
+    # once we would get to 7 we would return False
+    # no need to continue
 
 
+    print("False")
+    return True
+
+
+# Should be true
 is_first_come_first_served([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6])
+# Should be false
+is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 6, 3, 5])
 
 
-# --- notes about the question ---
-#
-#
-#
-#
+# ------------ notes about the question ----------
+# two registers - one for take out
+#               - one for customers eating inside
+# All the orders are combined into one list
+# they are handled FIFO (first in first out)
+# What we need to do: check that service is
+# order numbers are random, they do not need to be
+# in increasing order
 #
