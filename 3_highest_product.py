4de6daf7a15289c0005e66accc2be90d313da7c4
Andres Arizala <arizala13@gmail.com>
2020-10-19 13:30:36 -0500

    "Start of section 3 - Greedy. Added buy and sell stock question."

---
+++ 3_highest_product.py
@@ -0,0 +1,23 @@
+# --- general notes ---
+# Question: Given list of integers
+# find the highest product you can get
+# from three integers
+# Runtime: O(n)
+# Space complexity: O(1)
+
+def highest_product_of_3(list_of_ints):
+    # highest =
+    # lowest =
+    # high_product_of_2 =
+    # low_product_of_2 =
+
+highest_product_of_3([1, 2, 3, -40])
+
+
+# way to approach this is greedy algo
+# take product of two highest values multiple
+# same for lowest
+# then try the 3rd number with both and save that if
+# it is larger, continue to do so till the end is reached
+# so at the end you have two values, take the max of that
+
