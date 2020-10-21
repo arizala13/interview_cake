876b16f414441a8f20c1031d94dd1e3ff04448d2
Andres Arizala <arizala13@gmail.com>
2020-10-21 13:48:03 -0500

    "Setup for greedy question multiply all ints up to i except i"

---
+++ 3_product_of_all.py
@@ -0,0 +1,32 @@
+# --- general notes ---
+# Question: return a list of all the products of the
+# list except for that integer at that index
+# ex: [1, 7, 3, 4] -> [84, 12, 28, 21]
+#
+# Runtime:
+# Space complexity:
+
+def product_of_all(list_given):
+    # new array to save all answers
+    answer = []
+
+    # start pointer
+
+    # used to save the product of the values to the left of n
+
+    # used to save the product of the values to the right of n
+
+
+
+
+    return answer
+
+
+product_of_all([1, 7, 3, 4])
+
+
+# how to solve this?
+# look at n to end of list and multiply
+# look at start of list to n multiply that
+# multiply product_left * product_right
+# insert that in the correct place
